28521371ecdff21674d000609ec8a9242b350be3
cynex0 <m.orlov10@gmail.com>
2023-09-28 21:12:35 +0200

    remove comment

--- src/assignment2/Pokemon.java
+++ src/assignment2/Pokemon.java
@@ -1,22 +1,5 @@
 package assignment2;
 
-/*
-Each pokemon has a name, a maximum health points (MAX HP), energy points (EP), a single skill (Task 2), and a type.
-A pokemon can be one of four types: Fire, Water, Grass and Normal. When created, the EP always starts at 100, and the
-pokemon does not know any skill. On the other hand, the name, MAX HP and type must be specified.
-
-Pokemon also have current health points (HP), which can change, but not their maximum health points (MAX HP). In fact,
-a pokemon’s current HP cannot be less than zero, or greater than its MAX HP. Similarly, EP can never be less than zero
-or greater than 100. When creating a Pokemon, its current HP and current EP are the same value as their corresponding
-maximum HP and EP. From now, we use HP to refer to the current HP and MAX HP to refer to the maximum health points.
-
-After creation, we can change the name of a pokemon but not its type or MAX HP. Also, we cannot set its HP and EP to a
-specific value. HP and EP can only be changed as a consequence of battling (details in Tasks below).
-
-Two pokemons are equal if they have the same name, type, skill, HP, MAX HP, and EP. When printed, the pokemon should
-return one of two options below
-*/
-
 public class Pokemon {
     private static final int MAX_ENERGY = 100;
 
